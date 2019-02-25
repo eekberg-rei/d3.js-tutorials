9c2a029117f3855d5a5f3626189aa8a3f85a0260
Erik Ekberg <eekberg@rei.com>
2019-02-25 10:54:33 -0800

    A new single commit

--- 2 -- simple bar chart/index.js
+++ 2 -- simple bar chart/index.js
@@ -1,1 +1,12 @@
-alert('Hello!');
+const data = [30, 86, 168, 281, 303, 365];
+
+d3.select('.chart')
+        .select('div')
+        .data(data)
+                .enter()
+                .append('div')
+                .attr('class', 'chart-item')
+                .style('width', function (d) { return d + 'px'; } )
+                .style('background-color', 'steelblue')
+                .style('text-align', 'right')
+                .text(function (d) { return d; });
